56e4a93430b07e4cceea6a1ad1b9974617dbcaa2
jermp96 <jjeremyperez96@gmail.com>
2022-02-08 11:10:29 -0600

    *tabs sobre los ListTiles y estilo del AppBar

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -10,11 +10,17 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('listview tipo 2'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
       ),
       body: ListView.separated(
         itemBuilder: (context, index) => ListTile(
             title: Text(options[index]),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined, color: Colors.indigo,),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
           ),
         separatorBuilder: ( _, __) => const Divider(), // underscore cuando el parametro no se utilice. Valido para dart
         itemCount: options.length)
