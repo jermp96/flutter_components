2b152d97883c7c2082f861e03ea4c8741f7902b2
jermp96 <jjeremyperez96@gmail.com>
2022-02-16 11:55:23 -0600

    *cupertino dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,10 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('titulo'),
+            content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('Este es el contenido de la alerta'),
+                  SizedBox(height: 10),
+                  FlutterLogo(size: 100,)
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context) ,
+                  child: const Text('Cancelar', style: TextStyle(color: Colors.red),)),
+                TextButton(
+                  onPressed: () => Navigator.pop(context) ,
+                  child: const Text('OK'))
+              ],  
+          );
+        } 
+      
+      );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       barrierDismissible: false,
       context: context, 
@@ -23,7 +55,10 @@ class AlertScreen extends StatelessWidget {
               actions: [
                 TextButton(
                   onPressed: () => Navigator.pop(context) ,
-                  child: const Text('Cancelar'))
+                  child: const Text('Cancelar')),
+                TextButton(
+                  onPressed: () => Navigator.pop(context) ,
+                  child: const Text('OK'))
               ],
             );
           
@@ -36,7 +71,9 @@ class AlertScreen extends StatelessWidget {
     return  Scaffold(
       body: Center(
          child: ElevatedButton(
-           onPressed: () => displayDialog(context), 
+           onPressed: () => Platform.isAndroid 
+              ? displayDialogAndroid(context) 
+              : displayDialogIOS(context), 
            child: const Text('Mostrar alert')
            ),
 
