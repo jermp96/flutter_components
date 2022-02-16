3e6b0ef5002c8a3cc8cefd3d0846bade5f05dc25
jermp96 <jjeremyperez96@gmail.com>
2022-02-16 11:42:17 -0600

    *mostrar una alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,13 +3,40 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) {
+            return AlertDialog(
+              elevation: 5,
+              title: const Text('titulo'),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100,)
+              ]),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context) ,
+                  child: const Text('Cancelar'))
+              ],
+            );
+          
+        }
+      );
+  }
   
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Center(
          child: ElevatedButton(
-           onPressed: () {}, 
+           onPressed: () => displayDialog(context), 
            child: const Text('Mostrar alert')
            ),
 
@@ -17,9 +44,7 @@ class AlertScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.indigo,
         child: const Icon(Icons.close),
-        onPressed: () {
-            Navigator.pop(context);
-          }
+        onPressed: () => Navigator.pop(context)
         ),
     );
   }
